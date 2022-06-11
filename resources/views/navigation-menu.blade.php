4f1f4df300ea925788c3f421fdc8c53d0d7907e6
LoriV2 <94517486+LoriV2@users.noreply.github.com>
2022-06-11 12:33:18 +0200

    Update navigation-menu.blade.php

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -149,8 +149,15 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                {{ __('Przesyłanie') }}
             </x-jet-responsive-nav-link>
+        </div>
+        <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+            <div class="pt-2 pb-3 space-y-1">
+                <x-jet-responsive-nav-link href="{{ route('pliki') }}" :active="request()->routeIs('pliki')">
+                    {{ __('Pliki') }}
+                </x-jet-responsive-nav-link>
+            </div>
         </div>
 
         <!-- Responsive Settings Options -->
