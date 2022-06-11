dca8bc0fbe65fd7dce785f4f38e6d014afb82cd1
LoriV2 <94517486+LoriV2@users.noreply.github.com>
2022-06-11 10:18:52 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', function () {
     return ('welcome');
 });
 Route::middleware([
+    'scheme' => 'https',
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified'
@@ -30,7 +31,7 @@ Route::middleware([
         return view('pliki');
     })->name('pliki');
 });
-Route::middleware([])->group(function () {
+Route::middleware(['scheme' => 'https'])->group(function () {
     Route::get('/', function () {
         return view('welcome');
     })->name('welcome');
