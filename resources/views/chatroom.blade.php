564a4c5ff8f6ba4c8adb6b8b135f6bd5defaf4c8
LoriV2 <94517486+LoriV2@users.noreply.github.com>
2022-06-12 18:26:05 +0200

    Update chatroom.blade.php

--- resources/views/chatroom.blade.php
+++ resources/views/chatroom.blade.php
@@ -1,3 +1,11 @@
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+
+
 <script src="https://js.pusher.com/7.1/pusher.min.js"></script>
 <script>
     // Enable pusher logging - don't include this in production
@@ -29,7 +37,7 @@
         <p></p>
         @endforeach
     </div>
-    <div style="background-color:#8D8B8B">
+    <div style="background-color:#8D8B8B; width:30%">
         @auth
         <form method="POST" action="/sendverified">
             <p>Zacznij wpisywać wiadomość</p>
