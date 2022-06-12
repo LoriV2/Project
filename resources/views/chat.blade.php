457829567d364058f28ba1d6b03c9449e21ce368
LoriV2 <94517486+LoriV2@users.noreply.github.com>
2022-06-12 17:22:38 +0200

    chat

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -63,21 +63,7 @@
         });
     </script>
     </head>
-    @auth
-    <form method="POST" action="/sendverified">
-        <p>Zacznij wpisywać wiadomość</p>
-        <input type="text" name="message">
-        <input type="submit">
-        {{ csrf_field() }}
-    </form>
-    @else
-    <form method="POST" action="/sendnonverified">
-        <input type="text" name="user_name" placeholder="Wpisz swój nick">
-        <input type="text" name="message">
-        <input type="submit">
-        {{ csrf_field() }}
-    </form>
-    @endauth
+    
     
     <iframe src="https://heheeeee.herokuapp.com/chatroom" title="description">
     
--- resources/views/chatroom.blade.php
+++ resources/views/chatroom.blade.php
@@ -1,3 +1,20 @@
+@auth
+<form method="POST" action="/sendverified">
+    <p>Zacznij wpisywać wiadomość</p>
+    <input type="text" name="message">
+    <input type="submit">
+    {{ csrf_field() }}
+</form>
+@else
+<form method="POST" action="/sendnonverified">
+    <input type="text" name="user_name" placeholder="Wpisz swój nick">
+    <input type="text" name="message">
+    <input type="submit">
+    {{ csrf_field() }}
+</form>
+@endauth
+
+
 <div id="message-board">
     @foreach($messages as $message)
     <p>echo $message->User_name;
