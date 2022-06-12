750e3b841c8f386f5cd8be993fdcd09bb93486b8
LoriV2 <94517486+LoriV2@users.noreply.github.com>
2022-06-12 17:31:09 +0200

    Update chatroom.blade.php

--- resources/views/chatroom.blade.php
+++ resources/views/chatroom.blade.php
@@ -17,9 +17,9 @@
 
 <div id="message-board">
     @foreach($messages as $message)
-    <p>echo $message->User_name;
+    <p>{{$message->User_name}}
 
     </p>
-    <p>echo $message->message;</p>
+    <p>{{$message->message}}</p>
     @endforeach
 </div>
