576098b619571bf0484683f283bec36d80aff703
LoriV2 <94517486+LoriV2@users.noreply.github.com>
2022-06-12 16:44:24 +0200

    chat

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -59,28 +59,28 @@
 
         var channel = pusher.subscribe('message-board');
         channel.bind('Chat-Event', function(data) {
-
-        });
-        </script>
-        </head>
-        @auth
-        <form method="POST" action="/sendverified">
-            <p>Zacznij wpisywać wiadomość</p>
-            <input type="text" name="message">
-            <input type="submit">
-            {{ csrf_field() }}
-        </form>
-        @else
-        <form method="POST" action="/sendnonverified">
-            <input type="text" name="user_name" placeholder="Wpisz swój nick">
-            <input type="text" name="message">
-            <input type="submit">
-            {{ csrf_field() }}
-        </form>
-        @endauth
-        <div id="message-board">
             
-        </div>
+        });
+    </script>
+    </head>
+    @auth
+    <form method="POST" action="/sendverified">
+        <p>Zacznij wpisywać wiadomość</p>
+        <input type="text" name="message">
+        <input type="submit">
+        {{ csrf_field() }}
+    </form>
+    @else
+    <form method="POST" action="/sendnonverified">
+        <input type="text" name="user_name" placeholder="Wpisz swój nick">
+        <input type="text" name="message">
+        <input type="submit">
+        {{ csrf_field() }}
+    </form>
+    @endauth
+    <div id="message-board">
+
+    </div>
 
 </body>
 
