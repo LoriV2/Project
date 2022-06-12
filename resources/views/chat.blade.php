12bab9be569e3d2a407dcc56fe81584e4628c858
LoriV2 <94517486+LoriV2@users.noreply.github.com>
2022-06-12 12:19:44 +0200

    Update chat.blade.php

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -63,21 +63,21 @@
         });
         </script>
         </head>
-        @auth
+        
         <form method="POST" action="/sendverified">
             <p>Zacznij wpisywać wiadomość</p>
             <input type="text" name="message">
             <input type="submit">
             {{ csrf_field() }}
         </form>
-        @else
+        
         <form method="POST" action="/sendnonverified">
             <input type="text" name="user_name" placeholder="Wpisz swój nick">
             <input type="text" name="message">
             <input type="submit">
             {{ csrf_field() }}
         </form>
-        @endauth
+        
         <div id="message-board">
             
         </div>
